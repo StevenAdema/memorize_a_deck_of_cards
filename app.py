f0d1a7cac7d88e1ed5c3ceb196fb57af6bfcad44
Steven Adema <stevenadema@gmail.com>
2022-11-01 22:42:04 -0400

    adding pao table

--- app.py
+++ app.py
@@ -36,15 +36,12 @@ def memory_palace():
     cur.execute("SELECT * FROM card_system")
     df = pd.DataFrame(cur.fetchall())
     df = df.sample(frac=1).reset_index(drop=True) 
-    index = df.index.values.tolist()
-    first = [0,3,6,9,12,15,18,21,24,27,30,33,36,39,42,45,48,51]
-    second = [1,4,7,10,13,16,19,22,25,28,31,34,37,40,43,46,49,52]
     images = df[7]
     per = df[4]
     act = df[5]
     obj = df[6]
 
-    return render_template('index.html', data=zip(images,per,act,obj,index), first=first, second=second)
+    return render_template('memory_palace.html', data=zip(images,per,act,obj))
 
 @app.route('/deck_memorization')
 def deck_memorization():
@@ -75,6 +72,16 @@ def card_PAO_memorization():
 
     return render_template('card_PAO_memorization.html', data=zip(images,per,act,obj))
 
+@app.route('/PAO_system')
+def PAO_system():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM card_system")
+    df = pd.DataFrame(cur.fetchall())
+    df = df[[2,4,5,6]]
+    print(df)
+    df.columns = ['Card','Person','Action','Object']
+    return render_template('PAO_system.html', tables=[df.to_html(classes='data', index=False)], titles=df.columns)
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
