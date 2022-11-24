4f53c7b163d36e77bd5f35434edf13d55cd280d8
Steven Adema <stevenadema@gmail.com>
2022-11-23 22:18:51 -0500

    use of csv for testing

--- app.py
+++ app.py
@@ -16,70 +16,74 @@ mysql = MySQL(app)
 @app.route('/index')
 @app.route('/')
 def Index():
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM card_system")
-    df = pd.DataFrame(cur.fetchall())
+    # cur = mysql.connection.cursor()
+    # cur.execute("SELECT * FROM card_system")
+    df = pd.read_csv('./data/pao.csv', sep=',')
+    # df = pd.DataFrame(cur.fetchall())
     df = df.sample(frac=1).reset_index(drop=True) 
+    print(df)
     index = df.index.values.tolist()
     first = [0,3,6,9,12,15,18,21,24,27,30,33,36,39,42,45,48,51]
     second = [1,4,7,10,13,16,19,22,25,28,31,34,37,40,43,46,49,52]
-    images = df[7]
-    per = df[4]
-    act = df[5]
-    obj = df[6]
+    images = df['ImgLink']
+    per = df['Person']
+    act = df['Action']
+    obj = df['Object']
 
     return render_template('index.html', data=zip(images,per,act,obj,index), first=first, second=second)
 
 @app.route('/memory_palace')
 def memory_palace():
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM card_system")
-    df = pd.DataFrame(cur.fetchall())
+    # cur = mysql.connection.cursor()
+    # cur.execute("SELECT * FROM card_system")
+    df = pd.read_csv('./data/pao.csv', sep=',')
+    # df = pd.DataFrame(cur.fetchall())
     df = df.sample(frac=1).reset_index(drop=True) 
-    images = df[7]
-    per = df[4]
-    act = df[5]
-    obj = df[6]
+    images = df['ImgLink']
+    per = df['Person']
+    act = df['Action']
+    obj = df['Object']
 
     return render_template('memory_palace.html', data=zip(images,per,act,obj))
 
 @app.route('/deck_memorization')
 def deck_memorization():
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM card_system")
-    df = pd.DataFrame(cur.fetchall())
+    # cur = mysql.connection.cursor()
+    # cur.execute("SELECT * FROM card_system")
+    df = pd.read_csv('./data/pao.csv', sep=',')
+    # df = pd.DataFrame(cur.fetchall())
     df = df.sample(frac=1).reset_index(drop=True) 
     index = df.index.values.tolist()
     first = [0,3,6,9,12,15,18,21,24,27,30,33,36,39,42,45,48,51]
     second = [1,4,7,10,13,16,19,22,25,28,31,34,37,40,43,46,49,52]
-    images = df[7]
-    per = df[4]
-    act = df[5]
-    obj = df[6]
+    images = df['ImgLink']
+    per = df['Person']
+    act = df['Action']
+    obj = df['Object']
 
     return render_template('deck_memorization.html', data=zip(images,per,act,obj,index), first=first, second=second)
 
 @app.route('/card_PAO_memorization')
 def card_PAO_memorization():
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM card_system")
-    df = pd.DataFrame(cur.fetchall())
+    # cur = mysql.connection.cursor()
+    # cur.execute("SELECT * FROM card_system")
+    df = pd.read_csv('./data/pao.csv', sep=',')
+    # df = pd.DataFrame(cur.fetchall())
     df = df.sample(frac=1).reset_index(drop=True) 
-    images = df[7]
-    per = df[4]
-    act = df[5]
-    obj = df[6]
-    print(df)
+    images = df['ImgLink']
+    per = df['Person']
+    act = df['Action']
+    obj = df['Object']
 
     return render_template('card_PAO_memorization.html', data=zip(images,per,act,obj))
 
 @app.route('/PAO_system')
 def PAO_system():
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM card_system")
-    df = pd.DataFrame(cur.fetchall())
-    df = df[[2,4,5,6]]
-    print(df)
+    # cur = mysql.connection.cursor()
+    # cur.execute("SELECT * FROM card_system")
+    df = pd.read_csv('./data/pao.csv', sep=',')
+    # df = pd.DataFrame(cur.fetchall())
+    df = df[['CardRank','Person','Action','Object']]
     df.columns = ['Card','Person','Action','Object']
     return render_template('PAO_system.html', tables=[df.to_html(classes='data', index=False)], titles=df.columns)
 
--- test.py
+++ test.py
@@ -1,18 +1,28 @@
-from sqlalchemy.sql import select
-from sqlalchemy import create_engine, MetaData, Table
+from flask import Flask, render_template, request, url_for, flash
+from werkzeug.utils import redirect
+from flask_mysqldb import MySQL
+import pandas as pd
 
-CONN_STR = 'mysql://admin:1234@localhost:3306/deck_mem'
-engine = create_engine(CONN_STR, echo=True)
-metadata = MetaData()
-card_system = Table('card_system', metadata, autoload=True,
-                           autoload_with=engine)
-cols = card_system.c
+app = Flask(__name__)
+app.secret_key = 'many random bytes'
 
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '1234'
+app.config['MYSQL_DB'] = 'deck_mem'
 
-with engine.connect() as conn:
+mysql = MySQL(app)
 
-    query = (
-        select([cols.CardId, cols.CardRank, cols.CardSuit, cols.CardPerson, cols.CardAction, cols.CardObject])
-    )
-    for row in conn.execute(query):
-        print(row)
+# cur = mysql.connection.cursor()
+# cur.execute("SELECT * FROM card_system")
+df = pd.read_csv('./data/pao.csv', sep=',')
+# df = pd.DataFrame(cur.fetchall())
+df = df.sample(frac=1).reset_index(drop=True) 
+print(df)
+index = df.index.values.tolist()
+first = [0,3,6,9,12,15,18,21,24,27,30,33,36,39,42,45,48,51]
+second = [1,4,7,10,13,16,19,22,25,28,31,34,37,40,43,46,49,52]
+images = df['ImgLink']
+per = df['Person']
+act = df['Action']
+obj = df['Object']
