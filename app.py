9c1e1e6a2dbba6c49c50f3a26b22d2850a5a211c
Steven Adema <stevenadema@gmail.com>
2022-11-23 22:25:53 -0500

    toggle dev/live

--- app.py
+++ app.py
@@ -89,5 +89,6 @@ def PAO_system():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    # app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True)
 
